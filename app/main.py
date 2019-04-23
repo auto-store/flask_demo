9c3b540b2ff33c1dad4e810aebb324589598d167
Harris <thomash1@netapp.com>
2019-04-23 15:09:19 +0100

    add flash message

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("home.html")
-    
+
 @app.route('/background_process_test')
 def background_process_test():
     print "Hello"
@@ -14,6 +14,11 @@ def background_process_test():
 @app.route("/clickme")
 def clickme():
     return render_template("clickme.html")
+
+app.route('/flash')
+def flash():
+  message = request.args.get("msg")
+  return render_template("flash.html", ,msg=message)    
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0')
